bb0d56f726f87d8933ef55bcd5cb7539977836ff
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-25 19:47:21 -0300

    Update - correções
    Update - correções

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,11 @@ import { config } from './config/env';
 
 import app from './app';
 
+import { setupSwagger } from './config/swagger';
+
+setupSwagger(app);
+
 app.listen(config.port, () => {
     console.log(`Servidor executando em ${config.urlApi} (ambiente: ${config.nodeEnv})`);
+    console.log(`Documentação da API disponível em ${config.urlApi}/docs`);
 });
