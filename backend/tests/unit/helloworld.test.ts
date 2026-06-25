386c3a127f982ab20c14a75e91bf5f1424fcf9bb
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-25 18:53:45 -0300

    2 -
    2 -

---
+++ backend/tests/unit/helloworld.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Test inicial', () => {
+    // Testar a soma de dois números
+    it('deve somar dois numeros corretamente', () => {
+        const resultado = 1 + 1;
+        expect(resultado).toBe(2);
+    });
+})
