d8c39e3ba7c9f3ae15e1b282d8b6c13758f798ce
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-06-28 22:25:39 -0300

    updates
    updates

--- backend/src/modulos/planos-de-aula/plano-de-aula.rotas.ts
+++ backend/src/modulos/planos-de-aula/plano-de-aula.rotas.ts
@@ -1,12 +1,27 @@
-import { Router } from 'express';
+import { Router } from 'express'; 
 
-import { PlanoDeAulaControlador } from './plano-de-aula-controlador';
+import { PlanoDeAulaControlador } from './plano-de-aula-controlador'
 
 const planoDeAulaRotas = Router();
 
 const planoDeAulaControlador = new PlanoDeAulaControlador();
 
 // POST /planos-de-aula/rascunho
-planoDeAulaRotas.post('/rascunho', planoDeAulaControlador.gerarRascunho);
+planoDeAulaRotas.post(
+    '/rascunho',
+    planoDeAulaControlador.gerarRascunho
+);
+
+// POST /planos-de-aula/rascunho/melhorar
+planoDeAulaRotas.post(
+    '/rascunho/melhorar',
+    planoDeAulaControlador.melhorarRascunho
+);
+
+// POST /planos-de-aula/final
+planoDeAulaRotas.post(
+    '/final',
+    planoDeAulaControlador.gerarPlanoFinal
+);
 
 export { planoDeAulaRotas };
